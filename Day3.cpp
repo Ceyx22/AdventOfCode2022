969c6d7f20c6b3f5e5a048d0312bb552b7104c98
Fernando-Matias <fm278841@gmail.com>
2023-05-18 20:46:46 -0700

    Finished day 3

--- Day3.cpp
+++ Day3.cpp
@@ -12,122 +12,143 @@
 // Check if vector contains an element
 
 void Day3(){
-
+    int priorities = 0;
     std::ifstream datafile;
     std::string currLine;
     datafile.open(R"(\Users\fm278\CLionProjects\AdventOfCode\dataDayThree.txt)");
     std::map <char , int> sameChar;
 
     while (getline (datafile, currLine)) {
-        std::map <char , int> charIn1;
-        std::map <char , int> charIn2;
-
         int mid = (currLine.length()/2);
         std::string compartmentOne = currLine.substr(0, mid);
         std::string compartmentTwo = currLine.substr(mid, currLine.length());
-//        printf("C1 length: %i \n", compartmentOne.length());
-//        printf("C2 length: %i \n", compartmentTwo.length());
-        std::vector<char> key1 = stringToVector(compartmentOne);
-        std::vector<char> key2 = stringToVector(compartmentTwo);
-
-        for(int i = 0; i < mid; i++){
-            if(charIn1.find(compartmentOne[i]) != charIn1.end()) {
-                //value is found
-                charIn1.at(compartmentOne[i]) = charIn1.at(compartmentOne[i]) + 1;
-
-            }else {
-                //value not found
-                charIn1.insert({compartmentOne[i], 1});
-            }
-            if(charIn2.find(compartmentTwo[i]) != charIn2.end()) {
-                //value is found
-                charIn2.at(compartmentTwo[i]) = charIn2.at(compartmentTwo[i]) + 1;
-
-            }else {
-                //value not found
-                charIn2.insert({compartmentTwo[i], 1});
-            }
+        char ch = FindCommonLet(compartmentOne, compartmentTwo);
+        if (isupper(ch)){
+            priorities += int(ch) - 38;
+        }else {
+            priorities += int(ch) - 96;
         }
 
-        if()
-//        printf("Compartment 1: %s Compartment 2: %s \n", compartmentOne.c_str(), compartmentTwo.c_str());
-//        std::cout << numSameVal(key1, key2) << std::endl;
 
     }
-    //print_map(sameChar);
+    printf("sum %i \n", priorities);
 }
-
-template<typename K, typename V>
-void print_map(std::map<K, V> const &m){
-    for (auto const &pair: m) {
-        std::cout << "{" << pair.first << ": " << pair.second << "}\n";
-    }
-}
-int calcPriority(char x){
-    int priority = 0;
-    if(isupper(x)){
-        priority = x - 40;
-    }else{
-        priority = x - 96;
-    }
-    return priority;
-}
-std::vector<char> getAllKeys(std::map<char, int> map){
-    std::vector<char> keys;
-    for (auto it = map.begin(); it != map.end(); it++) {
-        keys.push_back(it->first);
-    }
-    return keys;
-}
-std::vector<int> getAllValues(std::map<char, int> map){
-    std::vector<int> value;
-    for (auto const& element : map) {
-        value.push_back(element.second);
-    }
-    return value;
-}
-
-std::map<char, int> compareVect(std::vector<char> key1, std::vector<char> key2, std::vector<int> val1, std::vector<int> val2){
-    std::map<char, int> sameChar;
-    for (char i : key1){
-        if (std::find(key2.begin(), key2.end(), i)) {
-            //Element found
-            int lowerNum = LowerNum(val1, val2, i);
-            sameChar.insert({key1.at(i), calcPriority(key1.at(i)) * lowerNum});
+char FindCommonLet(std::string c1, std::string c2){
+    for (auto &ch : c1) {
+        if(c2.find(ch) != std::string::npos){
+            return ch;
         }
     }
-    return sameChar;
+    return 0;
 }
-bool numSameVal(std::vector<char> vect1, std::vector<char> vect2){
-    for(int i = 0; i < vect2.size(); i++){
-        if (std::find(vect1.begin(), vect1.end(), vect2.at(i)) != vect1.end()) {
-            return true;
-        }
-    }
-    return false;
-}
-int LowerNum(std::vector<int> vect1, std::vector<int> vect2, int pos){
-    if(vect1.at(pos) < vect2.at(pos)){
-        return vect1.at(pos);
-    } else if(vect1.at(pos) > vect2.at(pos)){
-        return vect2.at(pos);
-    } else if(vect1.at(pos) == vect2.at(pos)){
-        return vect2.at(pos);
-    }else{
-        return 0;
-    }
-
-}
-void printVector(std::vector <char> const &a) {
-    std::cout << "The vector elements are : ";
-    for(char i : a)
-        std::cout << i << ' ';
-    printf("\n");
-}
-
-std::vector<char> stringToVector(std::string s){
-    std::vector<char> v;
-    std::copy(s.begin(), s.end(), std::back_inserter(v));
-    return v;
-}
+//std::map <char , int> listOfLettersInCommon(std::string l1, std::string l2){
+//    std::map<char, int> common;
+//    std::map<char, int> c1 = listOfLetters(l1);
+//    std::map<char, int> c2 = listOfLetters(l2);
+//    std::map<char, int>::iterator it;
+//    for (auto it2 = c1.begin(); it2 != c1.end(); it2++) {
+//        it = c2.find(it2->first);
+//        if (it != c2.end()){
+//            //found val
+//            int z = LowerNum(it -> second, it2 -> second);
+//            printf("Lowest Num: %i \n", z);
+//            common[it2 -> first] = z;
+//
+//        }
+//    }
+//    print_map(common);
+//    return common;
+//}
+//
+//std::map <char , int> listOfLetters(std::string list){
+//    std::map<char, int> let;
+//    std::map<char, int> let2;
+//    std::map<char, int>::iterator it;
+//    for(char i : list){
+//        it = let.find(i);
+//        if (it != let.end()){
+//            //found val
+//            let.at(i) = let.at(i) + 1;
+//        } else{
+//            let[i] = 1;
+//        }
+//    }
+//    for (auto it2 = let.begin(); it2 != let.end(); it2++) {
+//        int i = it2 -> second;
+//        if(i != 1){
+//            let2[it2->first] = it2->second;
+//        }
+//    }
+//    return let2;
+//}
+//
+//template<typename K, typename V>
+//void print_map(std::map<K, V> const &m){
+//    for (auto const &pair: m) {
+//        std::cout << "{" << pair.first << ": " << pair.second << "}\n";
+//    }
+//}
+//int calcPriority(std::map <char , int> sameChar){
+//    int priority = 0;
+//    for (auto it = sameChar.begin(); it != sameChar.end(); it++) {
+//        int i = it -> second;
+//        char c = it->first;
+//        if(isupper(c)){
+//            priority = priority + (c - 40)*i;
+//        }else{
+//            priority = priority + (c - 96)*i;
+//        }
+//    }
+//
+//
+//    return priority;
+//}
+//std::vector<char> getAllKeys(std::map<char, int> map){
+//    std::vector<char> keys;
+//    for (auto it = map.begin(); it != map.end(); it++) {
+//        keys.push_back(it->first);
+//    }
+//    return keys;
+//}
+//std::vector<int> getAllValues(std::map<char, int> map){
+//    std::vector<int> value;
+//    for (auto const& element : map) {
+//        value.push_back(element.second);
+//    }
+//    return value;
+//}
+//
+//bool numSameVal(std::vector<char> vect1, std::vector<char> vect2){
+//    for(int i = 0; i < vect2.size(); i++){
+//        if (std::find(vect1.begin(), vect1.end(), vect2.at(i)) != vect1.end()) {
+//            return true;
+//        }
+//    }
+//    return false;
+//}
+//int LowerNum(int val1, int val2){
+//    printf("Val 1: %i Val2: %i \n", val1, val2);
+//    if(val1 < val2){
+//        return val1;
+//    } else if (val1 > val2){
+//        return val2;
+//    } else if(val1 == val2){
+//        return val2;
+//    }else{
+//        return 0;
+//    }
+//
+//}
+//void printVector(std::vector <char> const &a) {
+//    std::cout << "The vector elements are : ";
+//    for(char i : a)
+//        std::cout << i << ' ';
+//    printf("\n");
+//}
+//
+//std::vector<char> stringToVector(std::string s){
+//    std::vector<char> v;
+//    std::copy(s.begin(), s.end(), std::back_inserter(v));
+//    return v;
+//}
 
--- Day3.h
+++ Day3.h
@@ -2,8 +2,7 @@
 // Created by fm278 on 5/4/2023.
 //
 
-#ifndef ADVENTOFCODE_DAY3_H
-#define ADVENTOFCODE_DAY3_H
+#pragma once
 #include <iostream>
 #include <tuple>
 #include <fstream>
@@ -13,16 +12,17 @@
 #include <algorithm>
 
 void Day3();
-template<typename K, typename V>
-void print_map(std::map<K, V> const &m);
+char FindCommonLet(std::string c1, std::string c2);
+//template<typename K, typename V>
+//void print_map(std::map<K, V> const &m);
+//
+//int calcPriority(std::map <char , int> sameChar);
+//std::map <char , int> listOfLettersInCommon(std::string l1, std::string l2);
+//std::map <char , int> listOfLetters(std::string list);
+//std::vector<char> getAllKeys(std::map<char, int> map);
+//std::vector<int> getAllValues(std::map<char, int> map);
+//bool numSameVal(std::vector<char> vect1, std::vector<char> vect2);
+//int LowerNum(int val1, int val2);
+//void printVector(std::vector <char> const &a);
+//std::vector<char> stringToVector(std::string s);
 
-int calcPriority(char x);
-std::vector<char> getAllKeys(std::map<char, int> map);
-std::vector<int> getAllValues(std::map<char, int> map);
-std::map<char, int> compareVect(std::vector<char> key1, std::vector<char> key2, std::vector<int> val1, std::vector<int> val2);
-bool numSameVal(std::vector<char> vect1, std::vector<char> vect2);
-int LowerNum(std::vector<int> vect1, std::vector<int> vect2, int pos);
-void printVector(std::vector <char> const &a);
-std::vector<char> stringToVector(std::string s);
-
-#endif //ADVENTOFCODE_DAY3_H
